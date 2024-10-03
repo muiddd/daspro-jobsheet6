7dd1abde642a81dfa1fab4d9890b2b9b069e7c7a
muiddd <muidabdulmuid99@gmail.com>
2024-10-03 07:58:49 +0700

    first commit

---
+++ Pemilihan2Percobaan101.java
@@ -0,0 +1,15 @@
+import java.util.Scanner ;
+public class Pemilihan2Percobaan101 {
+    public static void main(String[] args) {
+        Scanner sc01 = new Scanner(System.in) ;
+
+        System.out.print("Masukkan tahun: ");
+        int tahun = sc01.nextInt();
+
+        if ((tahun % 4) == 0) {
+            if ((tahun % 100)!= 0)
+                System.out.println("Tahun Kabisat");
+        } else
+            System.out.println("Bukan Tahun Kabisat");
+    }
+}
